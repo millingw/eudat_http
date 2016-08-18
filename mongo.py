ebfd34a1362dd2513ed72de999db33d075ef8245
millingw <m_illingworth@hotmail.com>
2016-08-18 12:23:36 +0100

    changed insert/update to use update/upsert

--- mongo.py
+++ mongo.py
@@ -22,21 +22,16 @@ def list_objects():
 # store metadata for an object
 # if the object already exists then the metadata will be updated,
 # otherwise a new object will be created.
-def store_metadata(object_id, metadata):
+def store_metadata(object_id, metadata, status=DEFAULTSTATUS):
     client = MongoClient()
     db = client[MONGODATABASENAME]
 
-    # look for an existing object
-    document = db.objects.find_one({'object_id': object_id})
+    db.objects.update(
+        {'object_id': object_id},
+        {'object_id': object_id, 'metadata': metadata, 'status': status},
+        upsert=True
+    )
 
-    # if no object found then insert a new document with default status
-    if document == None:
-         document = {'object_id': object_id, 'metadata': metadata, 'status': DEFAULTSTATUS}
-         db.objects.insert_one(document)
-    else:
-        # update an existing document
-        payload = {'metadata': metadata}
-        __update_document(id, payload)
 
 # get an object's metadata, or None if the object does not exist
 def get_metadata(object_id):
@@ -63,45 +58,26 @@ def get_object(object_id):
 
 # set the object status
 def set_status(object_id, status):
-    payload = { "status": status }
-    __update_document(object_id, payload)
 
-
-# update an existing object
-# payload is a dict containing the fields and values to update
-def __update_document(object_id, payload):
     client = MongoClient()
     db = client[MONGODATABASENAME]
-    result = db.update_one(
-    {"object_id": object_id},
-    {
-        "$set": payload,
-        "$currentDate": {"lastModified": True}
-    })
-    return result.modified_count
+    db.objects.update(
+        {'object_id': object_id},
+        {'status': status},
+        upsert=False
+    )
 
 
 # add or update an entity
 def update_entity(entity_id, filename):
     client = MongoClient()
     db = client[MONGODATABASENAME]
+    db.entities.update(
+        {'entity_id': entity_id},
+        {'filename': filename},
+        upsert=True
+    )
 
-     # look for an existing entity
-    document = db.entities.find_one({'entity_id': entity_id})
-
-    # if no entity found then insert a new document with default status
-    if document == None:
-         document = {'entity_id': entity_id, 'filename': filename}
-         db.entities.insert_one(document)
-    else:
-        # update an existing document
-         db.entities.update_one(
-             {"entity_id": entity_id},
-             {
-                "$set": { 'filename': filename},
-                "$currentDate": {"lastModified": True}
-             }
-         )
 
 # get information about an entity
 def get_entity(entity_id):
